8d413773c8b64a3dff3336a19d12103e7c3de97e
Ivan Vermeyen <ivan@vermeyen.org>
2020-03-09 19:10:55 +0100

    Add formFieldPrefixer helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,15 @@
 <?php
+
+if (! function_exists('formFieldPrefixer')) {
+    /**
+     * Create an instance of FormFieldPrefixer.
+     *
+     * @param string $prefix
+     *
+     * @return \CodeZero\FormFieldPrefixer\FormFieldPrefixer
+     */
+    function formFieldPrefixer($prefix = '')
+    {
+        return new \CodeZero\FormFieldPrefixer\FormFieldPrefixer($prefix);
+    }
+}
---
+++ tests/Unit/HelpersTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CodeZero\FormFieldPrefixer\Tests\Unit;
+
+use CodeZero\FormFieldPrefixer\FormFieldPrefixer;
+use PHPUnit\Framework\TestCase;
+
+class HelpersTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_form_field_prefixer_instance()
+    {
+        $prefixer = formFieldPrefixer();
+
+        $this->assertInstanceOf(FormFieldPrefixer::class, $prefixer);
+        $this->assertEquals('field', $prefixer->name('field'));
+    }
+
+    /** @test */
+    public function it_accepts_a_prefix()
+    {
+        $prefixer = formFieldPrefixer('prefix');
+
+        $this->assertInstanceOf(FormFieldPrefixer::class, $prefixer);
+        $this->assertEquals('prefix_field', $prefixer->name('field'));
+    }
+}
