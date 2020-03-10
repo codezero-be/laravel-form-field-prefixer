f2e1476e3972c34f01740196ba44d8f9a2f216ff
Ivan Vermeyen <ivan@vermeyen.org>
2020-03-10 16:10:56 +0100

    Remove unused files

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,22 +2,9 @@
 
 namespace CodeZero\FormFieldPrefixer\Tests;
 
-use CodeZero\FormFieldPrefixer\FormFieldPrefixerServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    /**
-     * Get the packages service providers.
-     *
-     * @param \Illuminate\Foundation\Application $app
-     *
-     * @return array
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            FormFieldPrefixerServiceProvider::class,
-        ];
-    }
+    //
 }
