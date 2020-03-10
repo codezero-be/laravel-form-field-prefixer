556e6d834c51f0449ea18c4c9da6c974884d61c8
Ivan Vermeyen <ivan@vermeyen.org>
2020-03-10 13:29:37 +0100

    Fix space

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,7 @@ namespace CodeZero\FormFieldPrefixer\Tests;
 use CodeZero\FormFieldPrefixer\FormFieldPrefixerServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
-abstract class TestCase extends  BaseTestCase
+abstract class TestCase extends BaseTestCase
 {
     /**
      * Get the packages service providers.
