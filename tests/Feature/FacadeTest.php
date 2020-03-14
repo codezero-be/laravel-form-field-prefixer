85d0c7b23911887185c35bd786c99501adbebf6a
Ivan Vermeyen <ivan@vermeyen.org>
2020-03-14 12:39:17 +0100

    Rename facade testcase

--- tests/Feature/FacadeTest.php
+++ tests/Feature/FacadeTest.php
@@ -2,27 +2,27 @@
 
 namespace CodeZero\FormFieldPrefixer\Tests\Feature;
 
-use CodeZero\FormFieldPrefixer\Facades\FormFieldPrefixer as FormFieldPrefixerFacade;
-use CodeZero\FormFieldPrefixer\FormFieldPrefixer;
+use CodeZero\FormFieldPrefixer\Facades\FormFieldPrefixer;
+use CodeZero\FormFieldPrefixer\FormFieldPrefixer as FormFieldPrefixerInstance;
 use CodeZero\FormFieldPrefixer\Tests\TestCase;
 
-class FormFieldPrefixerTest extends TestCase
+class FacadeTest extends TestCase
 {
     /** @test */
     public function it_makes_a_new_form_field_prefixer_instance()
     {
-        $prefixer = FormFieldPrefixerFacade::make();
+        $prefixer = FormFieldPrefixer::make();
 
-        $this->assertInstanceOf(FormFieldPrefixer::class, $prefixer);
+        $this->assertInstanceOf(FormFieldPrefixerInstance::class, $prefixer);
         $this->assertEquals('abc', $prefixer->name('abc', null));
     }
 
     /** @test */
     public function it_makes_a_new_form_field_prefixer_instance_with_prefix()
     {
-        $prefixer = FormFieldPrefixerFacade::make('prefix');
+        $prefixer = FormFieldPrefixer::make('prefix');
 
-        $this->assertInstanceOf(FormFieldPrefixer::class, $prefixer);
+        $this->assertInstanceOf(FormFieldPrefixerInstance::class, $prefixer);
         $this->assertEquals('prefix_abc', $prefixer->name('abc', null));
     }
 }
